ee7b2e8a827878c6a042bb7d2667e9484db4d1c2
kmolski <starfighter413@gmail.com>
2020-10-20 20:05:13 +0200

    Improve build.rs script for sha256_asm

--- sha256_asm/build.rs
+++ sha256_asm/build.rs
@@ -2,25 +2,24 @@ use std::env;
 use std::process::Command;
 
 fn main() {
+    // Rebuild if src/sha256sum.s is changed.
+    println!("cargo:rerun-if-changed=src/sha256sum.s");
+
     let out_dir = env::var("OUT_DIR").unwrap();
-    if !(Command::new("as")
-        .args(&["-o", &(out_dir.clone() + "/a.o"), "src/sha256_asm.s"])
+    if !(Command::new("gcc")
+        .args(&[
+            "-c",
+            "-o",
+            &(out_dir.clone() + "/libsha256.a"),
+            "src/sha256_asm.s",
+        ])
         .status()
         .unwrap()
-        .success()
-        && Command::new("ar")
-            .args(&[
-                "-crus",
-                &(out_dir.clone() + "/liba.a"),
-                &(out_dir.clone() + "/a.o"),
-            ])
-            .status()
-            .unwrap()
-            .success())
+        .success())
     {
         panic!("failed to build sha256sum.s!");
     }
 
     println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=a");
+    println!("cargo:rustc-link-lib=static=sha256");
 }
