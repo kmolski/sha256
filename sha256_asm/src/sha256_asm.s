755cf9ae343beb6b1342860146c769f28ba58fca
kmolski <starfighter413@gmail.com>
2020-10-23 16:58:36 +0200

    Remove redundant MOV instructions, improve comments

--- sha256_asm/src/sha256_asm.s
+++ sha256_asm/src/sha256_asm.s
@@ -14,15 +14,13 @@ loop:
     # let s0 = temp[0].rotate_right(2) ^ temp[0].rotate_right(13) ^ temp[0].rotate_right(22)
     mov r8d, dword ptr [rdi + 0]
     # r8d = temp[0]
-    rorx eax, r8d, 2
-    # eax = temp[0].rotate_right(2)
+    rorx r11d, r8d, 2
+    # r11d = temp[0].rotate_right(2)
     rorx r9d, r8d, 13
-    xor eax, r9d
-    # eax = temp[0].rotate_right(2) ^ temp[0].rotate_right(13)
+    xor r11d, r9d
+    # r11d = temp[0].rotate_right(2) ^ temp[0].rotate_right(13)
     rorx r10d, r8d, 22
-    xor eax, r10d
-    # eax  = s0 = temp[0].rotate_right(2) ^ temp[0].rotate_right(13) ^ temp[0].rotate_right(22)
-    mov r11d, eax
+    xor r11d, r10d
     # r11d = s0 = temp[0].rotate_right(2) ^ temp[0].rotate_right(13) ^ temp[0].rotate_right(22)
 
     # let maj = (temp[0] & temp[1]) ^ (temp[0] & temp[2]) ^ (temp[1] & temp[2])
@@ -41,18 +39,17 @@ loop:
     # eax = maj = (temp[0] & temp[1]) ^ (temp[0] & temp[2]) ^ (temp[1] & temp[2])
 
     # let temp2 = Wrap(s0) + Wrap(maj)
-    add eax, r11d
-    # eax = temp2 = Wrap(s0) + Wrap(maj) 
-    mov r11d, dword ptr [rdi + 28]
-    mov dword ptr [rdi + 28], eax
-    mov eax, r11d
+    add r11d, eax
+    # r11d = temp2 = Wrap(s0) + Wrap(maj)
+    mov eax, dword ptr [rdi + 28]
+    mov dword ptr [rdi + 28], r11d
     # eax = temp[7], temp[7] = temp2
 
     lea rdx, [rip + ROUND_VALUES]
     add eax, dword ptr [rdx + rcx * 4]
-    # eax = temp[7] + ROUND_VALUES[i]
+    # eax = Wrap(temp[7]) + Wrap(ROUND_VALUES[i])
     add eax, dword ptr [rsi + rcx * 4]
-    # eax = temp[7] + ROUND_VALUES[i] + w[i]
+    # eax = Wrap(temp[7]) + Wrap(ROUND_VALUES[i]) + Wrap(w[i])
     
     # let s1 = temp[4].rotate_right(6) ^ temp[4].rotate_right(11) ^ temp[4].rotate_right(25)
     mov r10d, dword ptr [rdi + 16]
@@ -66,21 +63,21 @@ loop:
     xor r9d, r11d
     # r9d = s1 = temp[4].rotate_right(6) ^ temp[4].rotate_right(11) ^ temp[4].rotate_right(25)
     add eax, r9d
-    # eax = temp[7] + ROUND_VALUES[i] + w[i] + s1
+    # eax = Wrap(temp[7]) + Wrap(ROUND_VALUES[i]) + Wrap(w[i]) + Wrap(s1)
     
     # let ch = (temp[4] & temp[5]) ^ (!temp[4] & temp[6])
     mov r9d, dword ptr [rdi + 20]
-    # r9d = temp[5]
+    # r9d = temp[5], r10d = temp[4]
     and r9d, r10d
     # r9d = (temp[4] & temp[5])
     mov r11d, dword ptr [rdi + 24]
-    # r11d = temp[6]
+    # r11d = temp[6], r10d = temp[4]
     andn r11d, r10d, r11d
     # r11d = (!temp[4] & temp[6])
     xor r9d, r11d
-    # r9d = (temp[4] & temp[5]) ^ (!temp[4] & temp[6])
+    # r9d = ch = (temp[4] & temp[5]) ^ (!temp[4] & temp[6])
     add eax, r9d
-    # eax = temp1 = Wrap(temp[7]) + Wrap(s1) + Wrap(ch) + Wrap(ROUND_VALUES[i]) + Wrap(w[i])
+    # eax = temp1 = Wrap(temp[7]) + Wrap(ROUND_VALUES[i]) + Wrap(w[i]) + Wrap(s1) + Wrap(ch)
 
     mov r15d, dword ptr [rdi + 28]
     mov r14d, dword ptr [rdi + 24]
@@ -134,7 +131,7 @@ loop:
 
     # The following round constants were taken from:
     # https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf (page 11)
-.align 64
+.balign 4
 ROUND_VALUES:
     .long 0x428A2F98,0x71374491,0xB5C0FBCF,0xE9B5DBA5,0x3956C25B,0x59F111F1,0x923F82A4,0xAB1C5ED5
     .long 0xD807AA98,0x12835B01,0x243185BE,0x550C7DC3,0x72BE5D74,0x80DEB1FE,0x9BDC06A7,0xC19BF174
