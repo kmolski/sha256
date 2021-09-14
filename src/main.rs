f5d22d4b7305a1e4cd8215cc0365ad8cf03f8614
kmolski <starfighter413@gmail.com>
2021-09-14 20:56:58 +0200

    Remove GUI, adjust Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,40 @@
+use std::env::args;
 use std::fmt::Write;
 use std::fs::File;
 use std::time::Instant;
 
-use glib::clone;
-use gtk::prelude::*;
+use rayon::iter::*;
 
 mod sha256_impl;
 use crate::sha256_impl::*;
 
-mod interface;
-use crate::interface::*;
+fn main() {
+    let file_names: Vec<String> = args().skip(1).collect();
 
-type HashResult = std::result::Result<(String, i32, u64), (String, i32)>;
+    file_names
+        .par_iter()
+        .map(|file_name| {
+            let file = match File::open(file_name) {
+                Ok(file) => file,
+                Err(e) => return Err(e),
+            };
 
-fn hash_files(
-    entries: Vec<(String, i32)>,
-    tx: glib::Sender<HashResult>,
-    use_asm: bool,
-    num_threads: usize,
-) {
-    fn get_hash_result(file_name: String, index: i32, use_asm: bool) -> HashResult {
-        let sha256_impl = if use_asm {
-            sha256_rounds_asm
-        } else {
-            sha256_rounds_rust
-        };
+            let mut ctx = SHA256Context::new(sha256_rounds_rust);
+            let hash = ctx.hash_file(file);
 
-        let file = File::open(&file_name).map_err(|err| (err.to_string(), index))?;
-        let mut ctx = SHA256Context::new(sha256_impl);
+            let mut hash_str = String::new();
+            for byte in hash.iter() {
+                hash_str.push_str(format!("{:02x}", byte).as_str());
+            }
 
-        let start = Instant::now();
-        let hash = ctx.hash_file(file);
-        let time = start.elapsed().as_millis() as u64;
-
-        let mut hash_str = String::new();
-
-        for byte in &hash {
-            write!(hash_str, "{:02x}", byte).map_err(|err| (err.to_string(), index))?;
-        }
-
-        Ok((hash_str, index, time))
-    }
-
-    let thread_pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(num_threads)
-        .build()
-        .expect("Failed to build thread pool");
-
-    for (file_name, index) in entries {
-        thread_pool.spawn(clone!(@strong tx => move || {
-            tx.send(get_hash_result(file_name, index, use_asm)).unwrap();
-        }));
-    }
-}
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-fn main() -> Result<()> {
-    gtk::init()?;
-
-    let glade_src = include_str!("./interface.ui");
-    let builder = gtk::Builder::from_string(glade_src);
-
-    let main_window = init_main_window(&builder)?;
-
-    let file_list: gtk::ListStore = builder
-        .get_object("file_list")
-        .ok_or("file_list not found")?;
-
-    let file_view: gtk::TreeView = builder
-        .get_object("file_view")
-        .ok_or("file_view not found")?;
-
-    let use_asm_btn: gtk::RadioButton = builder
-        .get_object("use_asm_btn")
-        .ok_or("use_asm_btn not found")?;
-
-    let thread_count_btn = init_thread_count_btn(&builder)?;
-    let progress_bar: gtk::ProgressBar = builder
-        .get_object("progress_bar")
-        .ok_or("progress_bar not found")?;
-
-    // let stop_signal = AtomicBool::new(false);
-
-    let add_files_btn = init_add_files_dialog(&builder, &main_window, &file_list)?;
-    let remove_files_btn = init_remove_files_btn(&builder, &file_view, &file_list)?;
-    let start_btn = init_start_btn(
-        &builder,
-        &use_asm_btn,
-        &thread_count_btn,
-        &progress_bar,
-        &file_list,
-    )?;
-    // let stop_btn = init_stop_btn(&builder, &stop_signal)?;
-    let save_results_btn = init_save_results_dialog(&builder, &main_window, &file_list)?;
-
-    gtk::main();
-    Ok(())
+            Ok((hash_str, file_name))
+        })
+        .for_each(|result| match result {
+            Ok((hash_str, file_name)) => {
+                println!("{}  {}", hash_str, file_name);
+            }
+            Err(e) => {
+                eprintln!("ERROR: {}", e);
+            }
+        });
 }
