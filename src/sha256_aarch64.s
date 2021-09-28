deee7b2d477a43850acc0418da1bace9a6a1004d
kmolski <starfighter413@gmail.com>
2021-09-28 14:23:46 +0200

    Add actual AArch64 implementation

--- src/sha256_aarch64.s
+++ src/sha256_aarch64.s
@@ -1,110 +1,81 @@
     .text
 
 .macro sha256_round a, b, c, d, e, f, g, h, i
-    mov eax, \e
-    ror eax, 6
-    mov edx, \e
-    ror edx, 11
-    xor eax, edx
-    mov ebx, \e
-    ror ebx, 25
-    xor eax, ebx
-    # eax = sigma1 = e.rotate_right(6) ^ e.rotate_right(11) ^ e.rotate_right(25)
+    mov w11, \e, ror #6
+    eor w11, w11, \e, ror #11
+    eor w11, w11, \e, ror #25
+    // w11 = sigma1 = e.rotate_right(6) ^ e.rotate_right(11) ^ e.rotate_right(25)
 
-    mov ebx, \e
-    and ebx, \f
-    mov edx, \e
-    not edx
-    and edx, \g
-    xor ebx, edx
-    # ebx = choice = (e & f) ^ (g & !e)
-    add \h, eax
-    add \h, ebx
-    lea rdx, [rip + ROUND_VALUES]
-    add \h, dword ptr [rdx + rcx * 4 + \i]
-    add \h, dword ptr [rsi + rcx * 4 + \i]
-    # \h = temp1 = h + sigma1 + choice + ROUND_VALUES[i] + w[i]
-    add \d, \h
-    # d = d + temp1
+    and w12, \e, \f
+    mvn w13, \e
+    and w13, w13, \g
+    eor w12, w12, w13
+    // w12 = choice = (e & f) ^ (g & !e)
+    add \h, \h, w11
+    add \h, \h, w12
+    // \h = h + sigma1 + choice
+    add x15, x2, x14
+    ldr w11, [x15, #\i]
+    add \h, \h, w11
+    add x16, x2, x1
+    ldr w12, [x16, #\i]
+    add \h, \h, w12
+    // \h = temp1 = h + sigma1 + choice + ROUND_VALUES[i] + w[i]
+    add \d, \d, \h
+    // d = d + temp1
 
-    mov eax, \a
-    ror eax, 2
-    mov edx, \a
-    ror edx, 13
-    xor eax, edx
-    mov ebx, \a
-    ror ebx, 22
-    xor eax, ebx
-    # eax = sigma0 = state[0].rotate_right(2) ^ state[0].rotate_right(13) ^ state[0].rotate_right(22)
-    mov edx, \a
-    and edx, \b
-    # edx = a & b
-    mov ebx, \a
-    and ebx, \c
-    xor edx, ebx
-    # edx = (a & b) ^ (a & c)
-    mov ebx, \b
-    and ebx, \c
-    xor edx, ebx
-    # edx = majority = (a & b) ^ (a & c) ^ (b & c)
-    add eax, edx
-    # eax = temp2 = sigma0 + majority
-    add \h, eax
-    # h = temp1 + temp2
+    mov w11, \a, ror #2
+    eor w11, w11, \a, ror #13
+    eor w11, w11, \a, ror #22
+    // w11 = sigma0 = state[0].rotate_right(2) ^ state[0].rotate_right(13) ^ state[0].rotate_right(22)
+    and w12, \a, \b
+    and w13, \a, \c
+    eor w12, w12, w13
+    // w12 = (a & b) ^ (a & c)
+    and w13, \b, \c
+    eor w12, w12, w13
+    // w12 = majority = (a & b) ^ (a & c) ^ (b & c)
+    add w11, w11, w12
+    // w11 = temp2 = sigma0 + majority
+    add \h, \h, w11
+    // h = temp1 + temp2
 .endm
 
 .global sha256_asm
 sha256_asm:
-    # arguments - rdi = state: *mut u32, rsi = w: *const u32
-    push r15
-    push r14
-    push r13
-    push r12
-    push rbx
-    mov rcx, 0
+    // arguments - x0 = state: *mut u32, x1 = w: *const u32
+    mov x2, xzr
+    adrp x14, ROUND_VALUES
+    add x14, x14, :lo12:ROUND_VALUES
 
-    mov r8d,  dword ptr [rdi + 0]
-    mov r9d,  dword ptr [rdi + 4]
-    mov r10d, dword ptr [rdi + 8]
-    mov r11d, dword ptr [rdi + 12]
-    mov r12d, dword ptr [rdi + 16]
-    mov r13d, dword ptr [rdi + 20]
-    mov r14d, dword ptr [rdi + 24]
-    mov r15d, dword ptr [rdi + 28]
+    ldp w3, w4,  [x0, #0]
+    ldp w5, w6,  [x0, #8]
+    ldp w7, w8,  [x0, #16]
+    ldp w9, w10, [x0, #24]
 sha256_asm_loop_start:
-    sha256_round r8d,  r9d,  r10d, r11d, r12d, r13d, r14d, r15d, 0
-    sha256_round r15d, r8d,  r9d,  r10d, r11d, r12d, r13d, r14d, 4
-    sha256_round r14d, r15d, r8d,  r9d,  r10d, r11d, r12d, r13d, 8
-    sha256_round r13d, r14d, r15d, r8d,  r9d,  r10d, r11d, r12d, 12
-    sha256_round r12d, r13d, r14d, r15d, r8d,  r9d,  r10d, r11d, 16
-    sha256_round r11d, r12d, r13d, r14d, r15d, r8d,  r9d,  r10d, 20
-    sha256_round r10d, r11d, r12d, r13d, r14d, r15d, r8d,  r9d,  24
-    sha256_round r9d,  r10d, r11d, r12d, r13d, r14d, r15d, r8d,  28
+    sha256_round w3,  w4,  w5,  w6,  w7,  w8,  w9,  w10, 0
+    sha256_round w10, w3,  w4,  w5,  w6,  w7,  w8,  w9,  4
+    sha256_round w9,  w10, w3,  w4,  w5,  w6,  w7,  w8,  8
+    sha256_round w8,  w9,  w10, w3,  w4,  w5,  w6,  w7,  12
+    sha256_round w7,  w8,  w9,  w10, w3,  w4,  w5,  w6,  16
+    sha256_round w6,  w7,  w8,  w9,  w10, w3,  w4,  w5,  20
+    sha256_round w5,  w6,  w7,  w8,  w9,  w10, w3,  w4,  24
+    sha256_round w4,  w5,  w6,  w7,  w8,  w9,  w10, w3,  28
 
-    add rcx, 8
-    cmp rcx, 64
-    jne sha256_asm_loop_start
+    add x2, x2, #32
+    cmp x2, #256
+    b.ne sha256_asm_loop_start
 
-    mov dword ptr [rdi + 28], r15d
-    mov dword ptr [rdi + 24], r14d
-    mov dword ptr [rdi + 20], r13d
-    mov dword ptr [rdi + 16], r12d
-    mov dword ptr [rdi + 12], r11d
-    mov dword ptr [rdi + 8],  r10d
-    mov dword ptr [rdi + 4],  r9d
-    mov dword ptr [rdi + 0],  r8d
-
-    pop rbx
-    pop r12
-    pop r13
-    pop r14
-    pop r15
+    stp w9, w10, [x0, #24]
+    stp w7, w8,  [x0, #16]
+    stp w5, w6,  [x0, #8]
+    stp w3, w4,  [x0, #0]
     ret
 
     .section .rodata
 
-    # The following round constants were taken from:
-    # https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf (page 11)
+    // The following round constants were taken from:
+    // https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf (page 11)
 .balign 4
 ROUND_VALUES:
     .long 0x428A2F98,0x71374491,0xB5C0FBCF,0xE9B5DBA5,0x3956C25B,0x59F111F1,0x923F82A4,0xAB1C5ED5
@@ -115,7 +86,3 @@ ROUND_VALUES:
     .long 0xA2BFE8A1,0xA81A664B,0xC24B8B70,0xC76C51A3,0xD192E819,0xD6990624,0xF40E3585,0x106AA070
     .long 0x19A4C116,0x1E376C08,0x2748774C,0x34B0BCB5,0x391C0CB3,0x4ED8AA4A,0x5B9CCA4F,0x682E6FF3
     .long 0x748F82EE,0x78A5636F,0x84C87814,0x8CC70208,0x90BEFFFA,0xA4506CEB,0xBEF9A3F7,0xC67178F2
-
-.balign 32
-INDEX_VECTOR:
-    .long 7,0,1,2,3,4,5,6
